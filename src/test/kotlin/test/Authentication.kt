22ae7f4d9b22b38efd5a6c3cbe50e3ff62524dc4
Bálint Böde <balint.bode@gmail.com>
2015-10-20 18:28:43 +0200

    Rename Auth test to Authentication

--- src/test/kotlin/test/Authentication.kt
+++ src/test/kotlin/test/Authentication.kt
@@ -3,7 +3,7 @@ package test
 import junit.framework.TestCase
 import net.solutinno.pocket.Pocket
 
-class Auth(name: String?) : TestCase(name) {
+class Authentication(name: String?) : TestCase(name) {
 
     val key: String = "46890-6b7d257a3df89fdc96e186f8"
 
@@ -14,14 +14,14 @@ class Auth(name: String?) : TestCase(name) {
         Pocket.init(key)
     }
 
-    fun testAuthentication () {
+    fun testRequest() {
         val result = Pocket.Authentication.request(redirect)
         println(result)
         assert(result.error == 0)
         println(Pocket.Authentication.getAuthorizeUrl(result.code, redirect))
     }
 
-    fun testAuthorization () {
+    fun testAuthorize() {
         val result = Pocket.Authentication.authorize("cc4d1f91-b127-1057-3398-43a8db")
         println(result)
         assert(result.error == 0)
