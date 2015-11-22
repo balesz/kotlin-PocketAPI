e34f661a3df591937853f92d68aaa2b07a4c49a4
Bálint Böde <balint.bode@gmail.com>
2015-11-22 18:49:45 +0100

    Rewrite Authenticate test with Rx

--- src/test/kotlin/test/Authenticate.kt
+++ src/test/kotlin/test/Authenticate.kt
@@ -1,29 +1,53 @@
 package test
 
-import junit.framework.TestCase
 import net.solutinno.pocket.Pocket
+import net.solutinno.pocket.model.AuthorizeResult
+import net.solutinno.pocket.model.RequestResult
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import rx.lang.kotlin.observable
+import rx.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class Authenticate(name: String?) : TestCase(name) {
+class Authenticate : Assert() {
 
     val key: String = "46890-6b7d257a3df89fdc96e186f8"
 
     val redirect: String = "http://www.solutinno.net"
 
-    override fun setUp() {
-        super.setUp()
+    @Before
+    fun setUp() {
         Pocket.init(key)
     }
 
-    fun testRequest() {
-        val result = Pocket.Authentication.request(redirect)
-        println(result)
-        assert(result.error == 0)
-        println(Pocket.Authentication.getAuthorizeUrl(result.code, redirect))
+    @Test
+    fun testAuthentication () {
+        observable<String> { it.onNext(redirect) }
+                .flatMap { Pocket.Authentication.rxRequest(it) }
+                .doOnNext { checkRequest(it) }
+                .doOnNext { authenticate(it.code) }
+                .delay(3, TimeUnit.SECONDS, Schedulers.immediate())
+                .flatMap { Pocket.Authentication.rxAuthorize(it.code) }
+                .doOnNext { checkAuthorize(it) }
+                .subscribe { println("Done") }
     }
 
-    fun testAuthorize() {
-        val result = Pocket.Authentication.authorize("cc4d1f91-b127-1057-3398-43a8db")
+    private fun checkRequest(request: RequestResult?) {
+        assertNotNull(request)
+        assert(request!!.error == 0)
+        println(request)
+    }
+
+    private fun authenticate (code: String) {
+        val command = "open ${Pocket.Authentication.getAuthorizeUrl(code, redirect)}"
+        Runtime.getRuntime().exec(command)
+        println(command)
+    }
+
+    private fun checkAuthorize(result: AuthorizeResult?) {
+        assertNotNull(result)
+        assert(result!!.error == 0)
         println(result)
-        assert(result.error == 0)
     }
 }
